7a02dfdb64268807c39c2f08177b3b24629f9867
Mickaël Canouil <mickael.canouil@cnrs.fr>
2021-01-05 17:40:02 +0000

    version 1.0.2

--- R/format_tag_content.R
+++ R/format_tag_content.R
@@ -5,10 +5,11 @@
 #'
 #' @keywords internal
 #' @usage NULL
+#' @noRd
 #'
 #' @return [data.frame]
 format_tag_content <- function(tag, content) {
-  if (nrow(content)==1 & is.na(content[1, 1])) {
+  if (nrow(content) == 1 & is.na(content[1, 1])) {
     output <- content
   } else {
     output <- as.data.frame(x = t(content[, -1, drop = FALSE]), stringsAsFactors = FALSE)
